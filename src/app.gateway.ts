a7203be1095788ee871dc8801f6ce987f9548a8f
seokunee <xocoolree@gmail.com>
2023-11-30 16:19:09 +0900

    [feat] 전체 소켓 idx , sockid 저장 #41
    Co-authored-by: SangWan Han <gtd9511@users.noreply.github.com>

--- src/app.gateway.ts
+++ src/app.gateway.ts
@@ -15,7 +15,6 @@ import { Logger, UnauthorizedException } from '@nestjs/common';
 import { Server, Socket } from 'socket.io';
 import { JoinRoomDto } from './game/dto/join.room.dto';
 import { UserStatus } from 'src/user/user.entity';
-
 @WebSocketGateway({ namespace: 'appGateway' })
 export class appGateway
   implements OnGatewayInit, OnGatewayConnection, OnGatewayDisconnect
@@ -27,38 +26,70 @@ export class appGateway
     private readonly userService: UserService,
   ) {}
 
+  onlineUsers: {
+    [key: number]: string;
+  } = {};
+
   afterInit() {}
 
   private logger = new Logger('games');
 
-  handleDisconnect(@ConnectedSocket() socket: Socket) {
+  async handleDisconnect(@ConnectedSocket() socket: Socket) {
+    const token = socket.handshake.auth.token || socket.handshake.query.token;
+    const userData = await this.authService.parsingJwtData(token);
+    const userIdx = userData.user_idx;
+    delete this.onlineUsers[userIdx];
+    try {
+      await this.userService.updateStatus(userIdx, UserStatus.OFFLINE);
+    } catch (error) {
+      this.logger.error(`${userIdx}의 offline 업데이트 실패`);
+    }
     this.logger.log('disconnected : ' + socket.id + ' in appGateway');
   }
 
   async handleConnection(@ConnectedSocket() socket: Socket) {
+    const token = socket.handshake.auth.token || socket.handshake.query.token;
+    const userData = await this.authService.parsingJwtData(token);
+    const userIdx = userData.user_idx;
+    this.onlineUsers[userIdx] = socket.id;
+
+    try {
+      await this.userService.updateStatus(userIdx, UserStatus.ONLINE);
+    } catch (error) {
+      this.logger.error(`${userIdx}의 offline 업데이트 실패`);
+    }
     this.logger.log('connected : ' + socket.id + ' in appGateway');
   }
 
-  // 프론트에서는 recieve challenge 이벤트 (id) =>
-  @SubscribeMessage('challengeGame')
-  async challengeGame(
+  // 챌린지 도전자 신청
+  @SubscribeMessage('checkEnableChallengeGame')
+  async checkEnableChallengeGame(
     @MessageBody() body: { requesterId: string; requestedId: string },
     @ConnectedSocket() socket: Socket,
   ) {
     const requester = await this.userService.getIsInclueGame(body.requesterId);
     const requested = await this.userService.getIsInclueGame(body.requestedId);
     if (requester.include || requested.include) {
-      socket.emit('challengeResult', { status: 'OFFLINE', success: false });
+      socket.emit('checkEnableChallengeGameSuccess', {
+        status: 'OFFLINE',
+        success: false,
+      });
       return;
     }
 
     if (requested.status === UserStatus.ONLINE) {
-      socket.emit('challengeResult', { status: 'ONLINE', success: true });
+      socket.emit('checkEnableChallengeGameSuccess', {
+        status: 'ONLINE',
+        success: true,
+      });
+      this.server
+        .to(this.onlineUsers[body.requestedId])
+        .emit('requestedChallenge', {});
     } else if (
       requested.status === UserStatus.OFFLINE ||
       requested.status === UserStatus.PLAYING
     ) {
-      socket.emit('challengeResult', {
+      socket.emit('checkEnableChallengeGameSuccess', {
         status: requested.status,
         success: false,
       });
